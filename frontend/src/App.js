a2632b8588f4b13c38a87d39789c63ff6bbe5738
Huseyn <huseyn.mikayil95@gmail.com>
2020-11-25 16:09:41 +0400

    import navbar component to header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <>
       <Header />
-      <main>
+      <main className='py-3'>
         <Container>
           <h1>Welcome To ProShop</h1>
         </Container>
