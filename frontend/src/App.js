b6c2fa2d9451f8eb28299d413d76a9e7c5c116e2
Huseyn <huseyn.mikayil95@gmail.com>
2020-11-26 17:19:56 +0400

    list all product items on homescreen component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (
     <>
       <Header />
-      <main className='py-3'>
+      <main className="py-3">
         <Container>
-          <h1>Welcome To ProShop</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
