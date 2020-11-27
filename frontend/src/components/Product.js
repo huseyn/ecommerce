c4c9f50e679c0160d2625a64b6a50445d40301a1
Huseyn <huseyn.mikayil95@gmail.com>
2020-11-27 12:50:08 +0400

    replace all anchor tags with Link and LinkContainer components in Product and Header components

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,32 +1,34 @@
+// eslint-disable-next-line quotes
 import React from "react";
-import { Card } from "react-bootstrap";
-import Rating from "./Rating";
+import {Link} from 'react-router-dom'
+import { Card } from 'react-bootstrap';
+import Rating from './Rating';
 
 const Product = ({ product }) => {
-  return (
-    <Card className="my-3 p-3 rounded">
-      <a href={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant="top" />
-      </a>
+    return (
+        <Card className="my-3 p-3 rounded">
+            <Link to={`/product/${product._id}`}>
+                <Card.Img src={product.image} variant="top" />
+            </Link>
 
-      <Card.Body>
-        <a href={`/product/${product._id}`}>
-          <Card.Title as="div">
-            <strong>{product.name}</strong>
-          </Card.Title>
-        </a>
+            <Card.Body>
+                <Link to={`/product/${product._id}`}>
+                    <Card.Title as="div">
+                        <strong>{product.name}</strong>
+                    </Card.Title>
+                </Link>
 
-        <Card.Text as="div">
-          <Rating
-            value={product.rating}
-            text={`${product.numReviews} reviews`}
-          />
-        </Card.Text>
+                <Card.Text as="div">
+                    <Rating
+                        value={product.rating}
+                        text={`${product.numReviews} reviews`}
+                    />
+                </Card.Text>
 
-        <Card.Text as="h3">${product.price}</Card.Text>
-      </Card.Body>
-    </Card>
-  );
+                <Card.Text as="h3">${product.price}</Card.Text>
+            </Card.Body>
+        </Card>
+    );
 };
 
 export default Product;
